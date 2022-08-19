53e076eafff85877388dbcbe9e662d2685165031
araelecs <aramirez48727@gmail.com>
2022-08-18 22:44:17 -0700

    Updating

--- w2learntogether.js
+++ w2learntogether.js
@@ -1,3 +1,9 @@
+const showOnPage = function (text) {
+    let newParagraph = document.createElement("p")
+    newParagraph.innerHTML = text
+    let outputDiv = document.getElementById("output")
+    outputDiv.append(newParagraph)
+  }
 let itIsMonday = false
 let itIsTuesday = true
 let itIsWednesday = false
@@ -9,10 +15,28 @@ let gasTankPercentage = 10
 
 //My daily tasks will depend on what day of the week it is.
 
-if(itIsTuesday === true){
-    showOnPage("Make sure roll trashcans to the curb. ") //Trash day is Wednesday, I like to roll out my trashcans the night before.
+showOnPage("<b>The list below consists of tasks you should complete today, have a great day!.<b> ")
+//I feel like this is too much code, but I cannot think of a better way to write it with what I currently know...
+if(itIsMonday === true){
+    showOnPage("Happy Monday!")
 }
-
+if(itIsTuesday === true) {
+    showOnPage("Happy Tuesday!")
+}
+if(itIsWednesday === true){
+    showOnPage("Happy Wednesday!")
+}
+if(itIsThursday === true) {
+    showOnPage("Happy Thursday!")}
+if(itIsFriday === true) {
+    showOnPage("Happy Friday!")
+}
+if (itIsSaturday === true){
+    showOnPage("Happy Saturday!")
+}
+if (itIsSunday === true) {
+    showOnPage("Happy Sunday!")
+}
 if (itIsMonday === true || itIsWednesday === true) {
     showOnPage("Good morning, you must attend CIT48B from 1:00PM to 3:20PM today.")
 } if(itIsTuesday === true) {
@@ -26,11 +50,13 @@ if (itIsSunday === true) {
 }
 //I make sure to prepare for the week on Sundays.
 
-
+if(itIsTuesday === true){
+    showOnPage("Make sure roll trashcans to the curb. ") //Trash day is Wednesday, I like to roll out my trashcans the night before.
+}
 if (gasTankPercentage >=50){
     showOnPage("You have enough gasoline to get you through the week.")
 } else if (gasTankPercentage <50 && gasTankPercentage >29 ){
         showOnPage("Consider filling up your gas tank in the upcoming days.")
     } else {
-        showOnPage("You're running low on gas, make sure to fill up today.")
-    }
+        showOnPage("You're running low on gas, make sure to fill up today.")}
+    
