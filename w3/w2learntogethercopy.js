0479bc2cfd5feba530d307ccb2bce72c0f362cd8
araelecs <aramirez48727@gmail.com>
2022-08-26 03:11:11 -0700

    W3 LearnTogether My Code

--- w3/w2learntogethercopy.js
+++ w3/w2learntogethercopy.js
@@ -1,62 +1,72 @@
+
 const showOnPage = function (text) {
     let newParagraph = document.createElement("p")
     newParagraph.innerHTML = text
     let outputDiv = document.getElementById("output")
     outputDiv.append(newParagraph)
   }
-let itIsMonday = false
-let itIsTuesday = true
-let itIsWednesday = false
-let itIsThursday = false
-let itIsFriday = false
-let itIsSaturday = false
-let itIsSunday = false
-let gasTankPercentage = 10
 
-//My daily tasks will depend on what day of the week it is.
+showOnPage("<b>This list consists of tasks you should complete today, which are based off the global variables below. Have a great day!.<b> ")
+showOnPage("Day of the week___>" + day)
+showOnPage("Gas Tank Level___>" + gasTankPercentage + " %")
+showOnPage("Do I need to go to the post office?___>" + postOfficeTrip)
+showOnPage("---END GLOBAL VARIABLE DATA---")
+showOnPage("<b>Today's Tasks<b>")
 
-showOnPage("<b>The list below consists of tasks you should complete today, have a great day!.<b> ")
-//I feel like this is too much code, but I cannot think of a better way to write it with what I currently know...
-if(itIsMonday === true){
-    showOnPage("Happy Monday!")
+let scheduledPlan = function (dayOfWeek, tankPercentage){
+    let gasTankPercentage = tankPercentage
+    let day = dayOfWeek
+    if (tankPercentage>50){
+        gasTankPercentage= "nearly full"
+        fillOrNot= "you have enough gasoline to get you through the week"
+    }else if (tankPercentage<50 && tankPercentage>29){
+        gasTankPercentage = "relatively full"
+        fillOrNot= "consider filling up soon."
+    }else {
+        gasTankPercentage = "nearly empty"
+        fillOrNot= "make sure to fill up today"
+    }
+    
+if(day===1){
+        day = "Monday"
+        coursework = "attend CIT48B from 1:00PM to 3:20PM today"        
+    } else if(day===2) {
+        day = "Tuesday"
+        coursework = "attend CIT-58F from 6:00PM to 9:50PM today"
+    } else if(day===3){
+        day = "Wednesday"
+        coursework = "attend CIT48B from 1:00PM to 3:20PM today"
+    } else if(day===4) {
+        day = "Thursday"
+        coursework = "work on CIT-93 and BA-28 coursework today"
+    } else if(day===5) {
+        day = "Friday"
+        coursework = "work on CIT-93 and BA-28 coursework today"
+    } else if (day===6){
+        day = "Saturday"
+        coursework = "work on CIT-93 and BA-28 coursework today"
+    } else {
+        day = "Sunday"
+        coursework = "set aside extra time to prepare for the upcoming school week"
+    }
+    return `Today is ${day}, you must ${coursework}. Your gas tank is ${gasTankPercentage}, ${fillOrNot}.`
 }
-if(itIsTuesday === true) {
-    showOnPage("Happy Tuesday!")
+  let result = scheduledPlan(7,20)
+
+    showOnPage(result)
+//Assuming the first day of the week is Monday ^^^
+let gasFillUp = function (gasTankPercentage) {
+    if (gasTankPercentage >=50){
+        showOnPage("You have enough gasoline to get you through the week.")
+}else if (gasTankPercentage <50 && gasTankPercentage >29){
+
+}else{
+    showOnPage("You're running low on gas, make sure to fill up today.")}
+    return `Your gas tank is ${gasTankPercentage}% full, ${fillOrNot}.`
 }
-if(itIsWednesday === true){
-    showOnPage("Happy Wednesday!")
-}
-if(itIsThursday === true) {
-    showOnPage("Happy Thursday!")}
-if(itIsFriday === true) {
-    showOnPage("Happy Friday!")
-}
-if (itIsSaturday === true){
-    showOnPage("Happy Saturday!")
-}
-if (itIsSunday === true) {
-    showOnPage("Happy Sunday!")
-}
-if (itIsMonday === true || itIsWednesday === true) {
-    showOnPage("Good morning, you must attend CIT48B from 1:00PM to 3:20PM today.")
-} if(itIsTuesday === true) {
-    showOnPage("Good morning, you must attend CIT-58F from 6:00PM to 9:50PM today.")
-}   else {
-    showOnPage("Good morning, you do not have a scheduled class today, you must work on CIT-93 and BA-28 coursework today.")
-}
+
 //I have a combination of in-person and onine classes, planning for them is important.
-if (itIsSunday === true) {
-    showOnPage("Make sure to set aside extra time prepare for the upcoming school week.")
-}
 //I make sure to prepare for the week on Sundays.
 
-if(itIsTuesday === true){
-    showOnPage("Make sure roll trashcans to the curb. ") //Trash day is Wednesday, I like to roll out my trashcans the night before.
-}
-if (gasTankPercentage >=50){
-    showOnPage("You have enough gasoline to get you through the week.")
-} else if (gasTankPercentage <50 && gasTankPercentage >29 ){
-        showOnPage("Consider filling up your gas tank in the upcoming days.")
-    } else {
-        showOnPage("You're running low on gas, make sure to fill up today.")}
+   
     
