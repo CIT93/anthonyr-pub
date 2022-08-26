8e52f42387173a8163936b26c07c507bd744be15
araelecs <aramirez48727@gmail.com>
2022-08-25 18:25:13 -0700

    Updating W2 Code

--- w2learntogether.js
+++ w2learntogether.js
@@ -4,55 +4,54 @@ const showOnPage = function (text) {
     let outputDiv = document.getElementById("output")
     outputDiv.append(newParagraph)
   }
-let itIsMonday = false
-let itIsTuesday = true
-let itIsWednesday = false
-let itIsThursday = false
-let itIsFriday = false
-let itIsSaturday = false
-let itIsSunday = false
-let gasTankPercentage = 10
 
 //My daily tasks will depend on what day of the week it is.
 
-showOnPage("<b>The list below consists of tasks you should complete today, have a great day!.<b> ")
+let day = "Tuesday"
+let gasTankPercentage = 50
+let postOfficeTrip = true
+
+showOnPage("<b>This list consists of tasks you should complete today, which are based off the global variables below. Have a great day!.<b> ")
+showOnPage("Day of the week___>" + day)
+showOnPage("Gas Tank Level___>" + gasTankPercentage + " %")
+showOnPage("Do I need to go to the post office?___>" + postOfficeTrip)
+showOnPage("---END GLOBAL VARIABLE DATA---")
+showOnPage("<b>Today's Tasks<b>")
+
 //I feel like this is too much code, but I cannot think of a better way to write it with what I currently know...
-if(itIsMonday === true){
+if(day === "Monday"){
     showOnPage("Happy Monday!")
-}
-if(itIsTuesday === true) {
-    showOnPage("Happy Tuesday!")
-}
-if(itIsWednesday === true){
-    showOnPage("Happy Wednesday!")
-}
-if(itIsThursday === true) {
-    showOnPage("Happy Thursday!")}
-if(itIsFriday === true) {
-    showOnPage("Happy Friday!")
-}
-if (itIsSaturday === true){
-    showOnPage("Happy Saturday!")
-}
-if (itIsSunday === true) {
-    showOnPage("Happy Sunday!")
-}
-if (itIsMonday === true || itIsWednesday === true) {
     showOnPage("Good morning, you must attend CIT48B from 1:00PM to 3:20PM today.")
-} if(itIsTuesday === true) {
+} else if(day === "Tuesday") {
+    showOnPage("Happy Tuesday!")
     showOnPage("Good morning, you must attend CIT-58F from 6:00PM to 9:50PM today.")
-}   else {
+    showOnPage("Make sure roll trashcans to the curb. ")
+} else if(day === "Wednesday"){
+    showOnPage("Happy Wednesday!")
+    showOnPage("Good morning, you must attend CIT48B from 1:00PM to 3:20PM today.")
+} else if(day === "Thursday") {
+    showOnPage("Happy Thursday!")
     showOnPage("Good morning, you do not have a scheduled class today, you must work on CIT-93 and BA-28 coursework today.")
+} else if(day === "Friday") {
+    showOnPage("Happy Friday!")
+    showOnPage("Good morning, you do not have a scheduled class today, you must work on CIT-93 and BA-28 coursework today.")
+} else if (day === "Saturday"){
+    showOnPage("Happy Saturday!")
+    showOnPage("Good morning, you do not have a scheduled class today, you must work on CIT-93 and BA-28 coursework today.")
+
+} else {
+    showOnPage("Happy Sunday!")
+    showOnPage("Make sure to set aside extra time prepare for the upcoming school week.")
+
 }
 //I have a combination of in-person and onine classes, planning for them is important.
-if (itIsSunday === true) {
-    showOnPage("Make sure to set aside extra time prepare for the upcoming school week.")
-}
 //I make sure to prepare for the week on Sundays.
-
-if(itIsTuesday === true){
-    showOnPage("Make sure roll trashcans to the curb. ") //Trash day is Wednesday, I like to roll out my trashcans the night before.
+if (postOfficeTrip === true){
+    showOnPage("Make sure to drop off packages at the post office.")
+}else{
+    showOnPage("You don't have any packages to drop off today.")
 }
+
 if (gasTankPercentage >=50){
     showOnPage("You have enough gasoline to get you through the week.")
 } else if (gasTankPercentage <50 && gasTankPercentage >29 ){
